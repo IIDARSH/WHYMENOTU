61dfac3472c63e13141ace95a3e456e675018000
IIDARSH <42307827+IIDARSH@users.noreply.github.com>
2018-08-12 04:27:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
+const prefix = '$'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
+client.user.setGame(`I need your hug`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -27,5 +27,449 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+
+
+
+
+
+
+
+client.on('message', message => {
+        var prefix = '$'; // هنا تقدر تغير البرفكس
+	var command = message.content.split(" ")[0];
+	if(command == prefix + 'bc') { // الكوماند !bc
+		var args = message.content.split(' ').slice(1).join(' ');
+		if(message.author.bot) return;
+		if(!args) return message.channel.send(`**➥ Useage:** ${prefix}bc كلامك`);
+		
+		let bcSure = new Discord.RichEmbed()
+		.setTitle(`:mailbox_with_mail: **هل انت متأكد انك تريد ارسال رسالتك الى** ${message.guild.memberCount} **عضو**`)
+		.setThumbnail(client.user.avatarURL)
+		.setColor('RANDOM')
+		.setDescription(`**\n:envelope: ➥ رسالتك**\n\n${args}`)
+		.setTimestamp()
+		.setFooter(message.author.tag, message.author.avatarURL)
+		
+		message.channel.send(bcSure).then(msg => {
+			msg.react('✅').then(() => msg.react('❎'));
+			message.delete();
+			
+			
+			let yesEmoji = (reaction, user) => reaction.emoji.name === '✅'  && user.id === message.author.id;
+			let noEmoji = (reaction, user) => reaction.emoji.name === '❎' && user.id === message.author.id;
+			
+			let sendBC = msg.createReactionCollector(yesEmoji);
+			let dontSendBC = msg.createReactionCollector(noEmoji);
+			
+			sendBC.on('collect', r => {
+				message.guild.members.forEach(member => {
+					member.send(args.replace(`[user]`, member)).catch();
+					if(message.attachments.first()){
+						member.sendFile(message.attachments.first().url).catch();
+					}
+				})
+				message.channel.send(`:timer: **يتم الان الارسال الى** \`\`${message.guild.memberCount}\`\` **عضو**`).then(msg => msg.delete(5000));
+				msg.delete();
+			})
+			dontSendBC.on('collect', r => {
+				msg.delete();
+				message.reply(':white_check_mark: **تم الغاء ارسال رسالتك بنجاح**').then(msg => msg.delete(5000));
+			});
+		})
+	}
+});
+
+
+
+
+
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'Hi') {
+    msg.reply('Bye');
+  }
+});
+
+
+
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'hi') {
+    msg.reply('Bye');
+  }
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'How are you') {
+    msg.reply('Good what about you');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'How are u') {
+    msg.reply('Good what about you');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'how are you') {
+    msg.reply('Good what about you');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'bad') {
+    msg.reply('Why Nigga');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Bad') {
+    msg.reply('Why Nigga');
+  }
+});
+
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'Good') {
+    msg.reply('okey,');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'good') {
+    msg.reply('Okey,');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'how are u') {
+    msg.reply('Good what about you');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'who are you') {
+    msg.reply('your mom');
+  }
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'Who are you') {
+    msg.reply('your mom');
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'who are u') {
+    msg.reply('your mom');
+  }
+});
+
+
+
+
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'Who are u') {
+    msg.reply('your mom');
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message =>{
+    let args = message.content.split(' ');
+    let prefix = '$'; 
+    
+    if(args[0] === `${prefix}avatar`){
+        let mentions = message.mentions.members.first()
+        if(!mentions) {
+          let sicon = message.author.avatarURL
+          let embed = new Discord.RichEmbed()
+          .setImage(message.author.avatarURL)
+          .setColor("#f7abab") 
+          .setDescription(`**${message.author.username}#${message.author.discriminator}**'s avatar :`);
+          message.channel.send({embed})
+        } else {
+          let sicon = mentions.user.avatarURL
+          let embed = new Discord.RichEmbed()
+          .setColor("#f7abab")
+          .setDescription(`**${mentions.user.username}#${mentions.user.discriminator}**'s avatar :`)
+          .setImage(sicon)
+          message.channel.send({embed})
+        }
+    };
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('guildMemberAdd', member => {
+    const botCount = member.guild.members.filter(m=>m.user.bot).size
+    const memberCount = [member.guild.memberCount] - [botCount]
+    client.channels.get('478001644148097034').setName(`⟫『 ${memberCount} Members 』⟪`);
+    client.channels.get('478001703531184160').setName(`⟫『 ${botCount} Bots 』⟪`);
+});
+
+client.on('guildMemberRemove', member => {
+    const botCount = member.guild.members.filter(m=>m.user.bot).size
+    const memberCount = [member.guild.memberCount] - [botCount]
+    client.channels.get('478001644148097034').setName(`⟫『 ${memberCount} Members 』⟪`);
+    client.channels.get('478001703531184160').setName(`⟫『 ${botCount} Bots 』⟪`);
+});
+
+
+
+
+
+client.on('message', message => {
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "ban") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
+  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+  /*let b5bzlog = client.channels.find("name", "5bz-log");
+  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
+  if (message.mentions.users.size < 1) return message.reply("**who wanna ban **");
+  if(!reason) return message.reply ("**اكتب سبب الطرد**");
+  if (!message.guild.member(user)
+  .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
+
+  message.guild.member(user).ban(7, user);
+
+  const banembed = new Discord.RichEmbed()
+  .setAuthor(`BANNED!`, user.displayAvatarURL)
+  .setColor("RANDOM")
+  .setTimestamp()
+  .addField("**اسم | Name**",  '**[ ' + `${user.tag}` + ' ]**')
+  .addField("**من قبل | By**", '**[ ' + `${message.author.tag}` + ' ]**')
+  .addField("**السبب | Reason**", '**[ ' + `${reason}` + ' ]**')
+  message.channel.send({
+    embed : banembed
+  })
+}
+});
+
+
+
+ client.on('message', message => {
+    var prefix = "$"
+var args = message.content.split(" ").slice(1);    
+if(message.content.startsWith(prefix + 'id')) {
+var year = message.author.createdAt.getFullYear()
+var month = message.author.createdAt.getMonth()
+var day = message.author.createdAt.getDate()
+var men = message.mentions.users.first();  
+let args = message.content.split(' ').slice(1).join(' ');
+if (args == '') {
+var z = message.author;
+}else {
+var z = message.mentions.users.first();
+}
+
+let d = z.createdAt;          
+let n = d.toLocaleString();   
+let x;                       
+let y;                        
+
+if (z.presence.game !== null) {
+y = `${z.presence.game.name}`;
+} else {
+y = "No Playing... |💤.";
+}
+if (z.bot) {
+var w = 'bot';
+}else {
+var w = 'member';
+}
+let embed = new Discord.RichEmbed()
+.setColor("#502faf")
+.addField('🔱| your name:',`**<@` + `${z.id}` + `>**`, true)
+.addField('🛡| Id:', "**"+ `${z.id}` +"**",true)
+.addField('♨| Playing:','**'+y+'**' , true)
+.addField('🤖| Your ac:',"**"+ w + "**",true)    
+.addField('📛| your code:',"**#" +  `${z.discriminator}**`,true)
+.addField('**THe date of your ac | 📆 **: ' ,year + "-"+ month +"-"+ day)    
+.addField("**Your date when you joined the server| ⌚   :**", message.member.joinedAt.toLocaleString())    
+
+.addField('**⌚ | The full date of your ac:**', message.author.createdAt.toLocaleString())
+.addField("**The last message | 💬  :**", message.author.lastMessage)            
+
+.setThumbnail(`${z.avatarURL}`)
+.setFooter(message.author.username, message.author.avatarURL)
+
+message.channel.send({embed});
+    if (!message) return message.reply('**ضع المينشان بشكل صحيح  ❌ **').catch(console.error);
+
+}
+
+});
+
+
+
+
+
+client.on('message', message => {
+if (message.content.startsWith("$kick")) {
+    var mention = message.mentions.members.first();
+    if(!mention) return message.channel.send("who wanna kick");
+
+    mention.kick("By: " + message.author.tag);
+    
+    message.channel.send("Yo Yo Done : " + mention.tag);
+};
+});
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+
+    if (message.content === ".mutechannel") {
+                        if(!message.channel.guild) return message.reply(' This command only for servers');
+
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' you don't have any prm');
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: false
+
+           }).then(() => {
+               message.reply("chat closed :white_check_mark: ")
+           });
+             }
+
+if (message.content === ".unmutechannel") {
+    if(!message.channel.guild) return message.reply(' This command only for servers');
+
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('you don't have any prm');
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: true
+
+           }).then(() => {
+               message.reply("chat opened :white_check_mark:")
+           });
+             }
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 client.login(process.env.BOT_TOKEN);
