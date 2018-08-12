b748785de941a3d68c35dafb8cc35103b847d369
IIDARSH <42307827+IIDARSH@users.noreply.github.com>
2018-08-12 16:41:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,10 +41,6 @@ client.user.setGame(`I need your hug`,"http://twitch.tv/S-F")
 
 
 
-
-
-
-
 client.on('message', message => {
         var prefix = '$'; // هنا تقدر تغير البرفكس
 	var command = message.content.split(" ")[0];
