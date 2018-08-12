98323ef38fe95a841ae888fd639fe77806b47f3c
IIDARSH <42307827+IIDARSH@users.noreply.github.com>
2018-08-12 04:29:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -456,7 +456,19 @@ if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('you d
 
 
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+  }
+});
 
 
 
