0d1c7fbf98d2ebd20a09d2dab7f7f251d2ba3143
IIDARSH <42307827+IIDARSH@users.noreply.github.com>
2018-08-12 04:58:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -488,11 +488,7 @@ client.on('message', message => {
   let command = message.content.split(" ")[0];
   command = command.slice(prefix.length);
 
-  let args = message.content.split(" ").slice(1);
-  if (command === "say") {
-          message.delete()
-    message.channel.sendMessage(args.join(" ")).catch(console.error);
-  }
+ 
   if (command == "say") {
    message.channel.sendMessage(args.join("  "))
   }
