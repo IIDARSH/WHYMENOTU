dd054c13a316b917b0f75d42c8d89d3cced0d3ba
IIDARSH <42307827+IIDARSH@users.noreply.github.com>
2018-08-13 22:02:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '$'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`R.I.P DARSH`,"http://twitch.tv/S-F")
+client.user.setGame(`*DARSH*`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
